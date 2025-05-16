7edb98eda29b3a0ba546dff7b45a3dd44356c4c1
Otavin23 <alvesotavio132@gmail.com>
2025-05-16 18:13:41 -0300

    feat: animations

--- components/ui/hero/index.tsx
+++ components/ui/hero/index.tsx
@@ -13,30 +13,57 @@ const Hero = () => {
       ease: "power1.inOut",
       stagger: 0.3,
     });
+
+    gsap.to(".hero-title", {
+      height: 200,
+    });
+
+    gsap.to(".hero-description", {
+      height: 100,
+    });
+
+    gsap.to(".hero-buttons", {
+      height: 100,
+      display: "flex",
+    });
+
+    gsap.to(".hero-buttons", {
+      height: 100,
+      display: "flex",
+    });
+
+    gsap.to(".hero-buttons button", {
+      height: 50,
+      display: "flex",
+    });
   }, []);
 
   return (
     <section className="flex justify-center items-center">
       <div className="max-w-[1800px] w-full mt-[10rem]">
-        <h1 className="hero-title text-8xl max-w-[900px] font-bold text-transparent">
+        <h1 className="hero-title text-8xl max-w-[900px] font-bold overflow-hidden h-0">
           Modern <span className="font-normal">payments</span> for markets
         </h1>
-        <p className="hero-description text-xl max-w-[800px] my-[1rem] text-transparent">
+        <p className="hero-description text-xl max-w-[800px] my-[1rem] overflow-hidden h-0">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, nostrum
           nisi porro dicta delectus adipisci aliquam similique aspernatur sed
           minima debitis? Culpa est natus error earum provident assumenda omnis
           quis!
         </p>
 
-        <div className="hero-buttons gap-[1rem] ">
+        <div className="hidden hero-buttons gap-[1rem]">
           <Button
             size="lg"
             variant="secondary"
-            className="rounded-xl mr-[1rem] bg-[#B1FF05]"
+            className="rounded-xl mr-[1rem] bg-[#B1FF05] overflow-hidden h-0 hidden"
           >
             Start now
           </Button>
-          <Button variant="outline" className="rounded-xl">
+
+          <Button
+            variant="outline"
+            className="rounded-xl overflow-hidden h-0 hidden"
+          >
             Learn more
           </Button>
         </div>
