9842925b7b33bac0c0ee206ce9aefb5321d22c44
Otavin23 <alvesotavio132@gmail.com>
2025-05-16 17:37:54 -0300

    faet: creating section hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Hero } from "@/components/ui/hero";
 import "./globals.css";
 import { Header } from "@/components/header/header";
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main className="">
       <Header />
+      <Hero />
     </main>
   );
 }
